994645e6e845ec5b8f3c27fac7042b4e99f0a193
Joshua M Granger <Joshua.Granger@ipaper.com>
2020-03-06 13:47:48 -0600

    More work on cover sheet

--- QAQC Buddy/Misc/CoverSheet.cs
+++ QAQC Buddy/Misc/CoverSheet.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using PdfSharp.Pdf;
+using ClosedXML;
+using ClosedXML.Excel;
 using QAQC_Buddy.Models;
 
 namespace QAQC_Buddy.Misc
@@ -22,12 +24,47 @@ namespace QAQC_Buddy.Misc
 
             // Check that the cover sheet template exists
             if (!File.Exists(Globals.PathCover))
-                return null;
+            {
+                Globals.ShowMsg("You selected to include a cover sheet; however, the cover sheet template could not be found.\n\n" +
+                    $"Expected Path: {Globals.PathCover}", "Error", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
 
-            // Open spreadsheet and add the items
+                return null;
+            }
+
+            // Check that the user did not try to include too many documents
+            if(names.Count > 24)
+            {
+                Globals.ShowMsg("It appears that you have included more than 24 documents. The cover sheet cannot handle this many documents. Please use less documents if you want an" +
+                    " automatically generated cover sheet.", "Warning", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Warning);
+
+                return null;
+            }
+
+
+            // Output File Information
+            // Assemble output filename and path
+            string targetFileExcel = $"{DateTime.Now.ToString("yyyyMMddHHmmss")}_{Environment.UserName.ToString()}.xlsx";
+            string targetPathExcel = Globals.PathFiles + targetFileExcel;
+            string targetFilePDF = $"{DateTime.Now.ToString("yyyyMMddHHmmss")}_{Environment.UserName.ToString()}.pdf";
+            string targetPathPDF = Globals.PathFiles + targetFilePDF;
+
+            // Check that the folder exists
+            if (!System.IO.Directory.Exists(Globals.PathFiles))
+                System.IO.Directory.CreateDirectory(Globals.PathFiles);
+
+
+            // Open cover spreadsheet and add the items
             if (names != null)
             {
-                
+                using (var workbook = new XLWorkbook(Globals.PathCover))
+                {
+                    var worksheet = workbook.Worksheet(0);
+
+                    for (int i = 0; i < names.Count; i++)
+                        worksheet.Cell(i + 7, 1).Value = names[i];
+
+                    workbook.SaveAs(targetFileExcel);
+                }
             }
 
             // Return result
